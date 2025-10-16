073811f508a01e68a5d982f64e5a083918838cf4
Keshav <keshavchaudhary0005@gmail.com>
2025-10-16 23:05:34 +0530

    Delete run_app.bat

--- run_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing dependencies...
-cd local_pulse_citizen
-flutter pub get
-echo.
-echo Dependencies installed! Now you can run:
-echo flutter run -t lib/main_full.dart -d RZCW114RM4D
-echo.
-pause
